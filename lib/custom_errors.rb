207662316379ba08bb3a2295fd01865ddbe53280
Daniel Sasser <danielbsasser@gmail.com>
2020-02-21 01:12:04 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,14 +7,20 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person 
+      raise PartnerError
+    else
+      person.partner = self
+    end
+    
+    class PartnerError < StandardError
+  
+    end
   end
 
 end
 
-class PartnerError < StandardError
-  
-end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
